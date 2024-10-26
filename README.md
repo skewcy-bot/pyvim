fded98b2e873395db8e196c2e0c1f91aa982aa26
Chuanyu Xue <chuanyu.xue@uconn.edu>
2024-10-26 14:11:17 -0400

    deploy demo

--- README.md
+++ README.md
@@ -1,29 +1,12 @@
 # Pyvim
-A vim emulator in Python
+
+Vim emulator written in pure Python code, based on regular expression.
+
+## Usage
+
+```bash
+python main.py
+```
 
 
-# Commands
 
-- [x]-[motion] h, j, k, l
-- [x]-[motion] w, W
-- [x]-[motion] e, E
-- [x]-[motion] b, B
-- [x]-[motion] ge, gE
-- [x]-[motion] $
-- [x]-[motion] ^
-- [x]-[motion] 0
-- [x]-[motion] gg
-- [x]-[motion] G
-- [x]-[motion] L, M, H
-- [x]-[motion] {}
-- [x]-[motion] g_
-- [x]-[motion] 5gg, 5G
-- [x]-[motion] f, F, t, T
-- [x]-[motion] ;, ,
-- [x]-[motion] zt, zz, zb
-- [x]-[motion] 10j, ..., 10k
-- [x]-[operator] i
-- [x]-[operator] r
-- [x]-[operator] J
-- [x]-[operator] R
-- [x]-[operator] i
--- web/web.py
+++ web/web.py
@@ -10,52 +10,37 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from pyvim.pyvim import VimEmulator
 from typing import Dict
 
+app = Flask(__name__, static_folder="static", template_folder="templates")
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+vim_instances: Dict[str, VimEmulator] = {}
 
-class PyVimWeb:
-    def __init__(self):
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        template_dir = os.path.join(current_dir, "templates")
-        static_dir = os.path.join(current_dir, "static")
 
-        print(f"Current directory: {current_dir}")
-        print(f"Template directory: {template_dir}")
-        print(f"Static directory: {static_dir}")
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-        self.app = Flask(
-            __name__, template_folder=template_dir, static_folder=static_dir
-        )
 
-        CORS(self.app)
-        self.socketio = SocketIO(self.app, cors_allowed_origins="*")
-        self.vim_instances: Dict[str, VimEmulator] = {}
+@app.route("/static/<path:filename>")
+def serve_static(filename):
+    return send_from_directory(app.static_folder, filename)
 
-        self.setup_routes()
-        self.setup_socketio()
 
-    def setup_routes(self):
-        @self.app.route("/")
-        def index():
-            return render_template("index.html")
+@socketio.on("connect")
+def handle_connect():
+    session_id = request.sid
+    vim_instances[session_id] = VimEmulator("hello world\n", 0, 0, web_mode=True)
+    emit("update", {"output": "Press any key to start"})
 
-        @self.app.route('/static/<path:filename>')
-        def serve_static(filename):
-            return send_from_directory(self.app.static_folder, filename)
 
-    def setup_socketio(self):
-        @self.socketio.on("connect")
-        def handle_connect():
-            session_id = request.sid
-            self.vim_instances[session_id] = VimEmulator(
-                "hello world\n", 0, 0, web_mode=True
-            )
-            emit("update", {"output": "Press any key to start"})
+@socketio.on("command")
+def handle_command(command):
+    session_id = request.sid
+    vim = vim_instances[session_id]
+    output = vim.exec(command)
+    emit("update", {"output": output})
 
-        @self.socketio.on("command")
-        def handle_command(command):
-            session_id = request.sid
-            vim = self.vim_instances[session_id]
-            output = vim.exec(command)
-            emit("update", {"output": output})
 
-    def run(self, debug=True, host="0.0.0.0", port=4399):
-        self.socketio.run(self.app, debug=debug, host=host, port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    socketio.run(app, host="0.0.0.0", port=port)
